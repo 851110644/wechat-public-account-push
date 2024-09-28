bd84bf8f4aaf8af18c39dab8efc1a4a15124425e
851110644 <851110644@qq.com>
2024-09-28 14:24:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -12,7 +12,8 @@ const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'bc56e2a2e4aba2980866b7776495289f',
-
+  PROVINCE: '浙江',
+  CITY: '杭州',
   
   USERS: [
     {
@@ -22,8 +23,7 @@ const USER_CONFIG = {
       id: 'ouEaB6DYtsYo49bFN2Y_qkyOSfRI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'xXjR1_B4nBF6pKCK_ZY6pvrXAryfanARNZauZ3h7sNI',
-      PROVINCE: '浙江',
-      CITY: '金华',
+   
 
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
