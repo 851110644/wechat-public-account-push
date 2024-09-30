55e3b13673c3cf7557cc9e53ded4930c6d20ed00
851110644 <851110644@qq.com>
2024-09-30 14:13:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ouEaB6DYtsYo49bFN2Y_qkyOSfRI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'z4zK10AGYIG1aJoegZ9UYhjKK0TW-RltRUp_pC66E9I',
+      useTemplateId: 'YM97jRrzqc5hbnX6PrGJAjL0yhJ3HCJEqOTsp7TBr_w',
    
 
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
