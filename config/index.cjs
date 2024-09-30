82c4285f61458929941d33cfcd34dba4287e8e70
851110644 <851110644@qq.com>
2024-09-30 21:12:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -45,9 +45,9 @@ SWITCH: {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ouEaB6DYtsYo49bFN2Y_qkyOSfRI',
+      id: 'ouEaB6NtMbAlweKKSCM7dEO0W8Fc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'HomVc14TAchQAJjOn0B29pRkRJ42_LsZampWkJjWoQU',
+      useTemplateId: 'g--X1j9L0Z5qi93k1AeCGt5Q6QSnGAaNnoX6j9ZQkgs',
    
       courseSchedule: {
         // 单双周的基准
