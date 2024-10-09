34f1129ac693ece03309ab37e72c8e8133a467ac
851110644 <851110644@qq.com>
2024-10-09 21:07:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -48,7 +48,7 @@ SWITCH: {
        //id: 'ouEaB6NtMbAlweKKSCM7dEO0W8Fc',
        id: 'ouEaB6DYtsYo49bFN2Y_qkyOSfRI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'emm4ZdwnnceZ9shltzeKpt6q1mHsYC9YVzhHPJo7Nrc',
+      useTemplateId: 'QGw1FFC0PEXfnSlhzK1LBKhpKj7CaHs2vFGVwYRO_2g',
    
       courseSchedule: {
         // 单双周的基准
